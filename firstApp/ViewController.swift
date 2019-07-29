8ec09b6b32708cd407518b6d30ab44a4e2d62dbd
Gabby Ya Vele <gabbyvele@gmail.com>
2019-07-29 13:27:25 +0200

    adding files

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by Gabriel Matshabe on 2019/07/13.
+//  Copyright © 2019 Gabriel Matshabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
